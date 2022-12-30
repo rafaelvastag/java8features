932acd75c3e8a92c1a55aa45f2aeb194c43ac41c
rafaelvastag <rgvastag@gmail.com>
2022-12-30 05:50:44 -0800

    Functional Interfaces

--- java8/src/main/java/org/vastag/features/java8/Main.java
+++ java8/src/main/java/org/vastag/features/java8/Main.java
@@ -2,6 +2,6 @@ package org.vastag.features.java8;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Java 8 - new features");
     }
 }
