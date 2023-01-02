82959005a8a2209e9a7c84b0ce68b9bf8ad08ff2
rafaelvastag <rgvastag@gmail.com>
2023-01-02 11:55:32 -0300

    consumer operation

--- java8/src/main/java/org/vastag/features/java8/examples/functionalInterfaces/ConsumerExample.java
+++ java8/src/main/java/org/vastag/features/java8/examples/functionalInterfaces/ConsumerExample.java
@@ -1,10 +1,43 @@
 package org.vastag.features.java8.examples.functionalInterfaces;
 
+import org.vastag.features.java8.examples.data.Student;
+import org.vastag.features.java8.examples.data.StudentDataBase;
+
 import java.util.function.Consumer;
 
 public class ConsumerExample {
+
+    static Consumer<String> c1 = (s) -> System.out.println(s.toUpperCase());
+    static Consumer<Student> c2 = (student) -> System.out.println(student);
+    static Consumer<Student> c3 = (student) -> System.out.println(student.getName());
+    static Consumer<Student> c4 = (student) -> System.out.println(student.getActivities());
+
     public static void main(String[] args) {
-        Consumer<String> c1 = (s) -> System.out.println(s.toUpperCase());
         c1.accept("touppercase java 8+ version");
+        printStudents();
+        printNameAndActivities();
+        printNameAndActivitiesWithCondition();
+    }
+
+    public static void printStudents() {
+        System.out.println("printStudents: ");
+        var students = StudentDataBase.getAllStudents();
+        students.forEach(c2);
+    }
+
+    public static void printNameAndActivities() {
+        System.out.println("printNameAndActivities: ");
+        var students = StudentDataBase.getAllStudents();
+        students.forEach(c3.andThen(c4));
+    }
+
+    public static void printNameAndActivitiesWithCondition() {
+        System.out.println("printNameAndActivitiesWithCondition: ");
+        var students = StudentDataBase.getAllStudents();
+        students.forEach(student ->
+        {
+            if (student.getGradeLevel() >= 3 && student.getGpa() >= 3.9)
+                c3.andThen(c4).accept(student);
+        });
     }
 }
