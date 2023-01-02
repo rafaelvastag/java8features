f1bcbb31bed4cccd37167f77b50325f50a8734b0
rafaelvastag <rgvastag@gmail.com>
2023-01-02 09:11:18 -0300

    add git .ignore

--- java8/src/main/java/org/vastag/features/java8/examples/functionalInterfaces/ConsumerExample.java
+++ java8/src/main/java/org/vastag/features/java8/examples/functionalInterfaces/ConsumerExample.java
@@ -5,6 +5,6 @@ import java.util.function.Consumer;
 public class ConsumerExample {
     public static void main(String[] args) {
         Consumer<String> c1 = (s) -> System.out.println(s.toUpperCase());
-        c1.accept("touppercase");
+        c1.accept("touppercase java 8+ version");
     }
 }
